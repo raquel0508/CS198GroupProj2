96f4d260240c747500dbc3a802e3ac0445a4ff87
Simon Cao <scsmncao@gmail.com>
2016-04-14 14:34:40 -0700

    home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+
+  root to:"home#index"
   
+  get 'home/index'
+
   devise_for :users
   get 'users/new'
 
   resources :users
   resources :recipes
-  root to:"homes#index"
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< Updated upstream
-ActiveRecord::Schema.define(version: 20160414205406) do
-=======
 ActiveRecord::Schema.define(version: 20160414210418) do
->>>>>>> Stashed changes
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
