198a50a207f72c27074baf1645a33e867d5b3649
Simon Cao <scsmncao@gmail.com>
2016-04-24 16:19:53 -0700

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160424060002) do
+ActiveRecord::Schema.define(version: 20160424214156) do
 
   create_table "friends", force: :cascade do |t|
     t.integer  "user_id"
