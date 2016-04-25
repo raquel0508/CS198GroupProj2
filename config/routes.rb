8b0f88993169b6e823b5b95148d35ffe114c61c9
Raquel Layug <raquel0508@berkeley.edu>
2016-04-24 19:37:41 -0700

    search works, but not finding the search

--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,14 @@ Rails.application.routes.draw do
   devise_for :users
   get 'users/new'
 
+  patch 'like', to:'recipes#like'
+  get '/recipes/search', to: 'recipes#search', as: :search
+  get '/recipes/found', to: 'recipes#found', as: :found
+  get '/recipes/new', to: 'recipes#new'
+  post 'recipes', to: 'recipes#create'
+
   resources :users
   resources :recipes
   resources :ingredients
   resources :recipesteps
-
-  patch 'like', to:'recipes#like'
-  get '/recipes/search', to: 'recipes#search', as: :search
-  get '/recipes/new', to: 'recipes#new'
-  post 'recipes', to: 'recipes#create'
-
-
 end
