1fefba53d1fa01762dfb1e47a8b20138205781fd
Simon Cao <scsmncao@gmail.com>
2016-04-24 23:01:05 -0700

    fixed some bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-  attr_accessor :email
+  attr_accessor :login
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
